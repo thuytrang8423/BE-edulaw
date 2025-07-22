fb0fb772e9ddfdbd1c404aad69be75c633e908ba
Miru UwU <miruhinana@gmail.com>
2025-07-22 19:54:21 +0700

    Update legalClauses.js

--- routes/legalClauses.js
+++ routes/legalClauses.js
@@ -4,10 +4,11 @@ const controller = require('../controllers/LegalClauseController');
 
 router.post('/', controller.create);
 router.get('/', controller.getAll);
+router.get('/search', controller.search);
 router.get('/:id', controller.getById);
 router.put('/:id', controller.update);
 router.delete('/:id', controller.delete);
-router.get('/search', controller.search);
+
 module.exports = router;
 
 
